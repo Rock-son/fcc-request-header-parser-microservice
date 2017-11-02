c4f76529528bbd5a5c88069f2dc648743e37ebf2
Rok <Rok>
2017-11-02 09:44:51 +0100

    changed IP method

--- server.js
+++ server.js
@@ -4,15 +4,12 @@
 var server = http.createServer(function(req, res) {
 
     if (req.url === "/") {
-    var IP = req.headers.host,
+    var IP = req.connection.remoteAddress,
         language = /^.*?(\,)/.exec(req.headers["accept-language"])[0].replace(/\,/, ""),
         OS = /\((.*)\)/.exec(req.headers["user-agent"])[0].replace(/\(/, "").replace(/\)/, "");
-        //language = req.headers["accept-language"].split(",")[0],
-        //OS = req.headers["user-agent"];
 
     res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end("This is stupid!");
-    res.end(JSON.stringify({ip: "test", language: language, software: OS}));
+    res.end(JSON.stringify({ip: IP, language: language, software: OS}));
     }
 });
 
