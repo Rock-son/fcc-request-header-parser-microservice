f779fefdf930869323a34c7f3725abdef3b28d23
Rok <Rok>
2017-11-02 08:28:50 +0100

    initial commit

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var http = require("http"),
 var server = http.createServer(function(req, res) {
 
     var IP = req.headers.host,
-        language = /^.*?(\,)/.exec(req.headers["accept-language"])[0].replace(/\,/, ""),
-        OS = /\((.*)\)/.exec(req.headers["user-agent"])[0].replace(/\(/, "").replace(/\)/, "");
+        //language = /^.*?(\,)/.exec(req.headers["accept-language"])[0].replace(/\,/, ""),
+        //OS = /\((.*)\)/.exec(req.headers["user-agent"])[0].replace(/\(/, "").replace(/\)/, "");
+        language = req.headers["accept-language"].split(",")[0],
+        OS = req.headers["user-agent"];
 
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.end(JSON.stringify({ip: IP, language: language, software: OS}));
